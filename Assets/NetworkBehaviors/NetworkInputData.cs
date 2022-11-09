90fc84d293cba2635b07405b1230a58e271d161a
roland-vidar <shanlogauthier@gmail.com>
2022-11-08 23:24:32 -0300

    Reimplement spawner on scene

--- Assets/NetworkBehaviors/NetworkInputData.cs
+++ Assets/NetworkBehaviors/NetworkInputData.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 
 public struct NetworkInputData : INetworkInput
 {
-    public const byte MOUSEBUTTON1 = 0x01;
-
-    public byte buttons;
     public Vector3 direction;
 }
---
+++ Assets/Player_/Player.cs
@@ -0,0 +1,24 @@
+using Fusion;
+using UnityEngine;
+
+namespace Player_
+{
+    public class Player : NetworkBehaviour
+    {
+        private NetworkCharacterControllerPrototype _cc;
+
+        private void Awake()
+        {
+            _cc = GetComponent<NetworkCharacterControllerPrototype>();
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (GetInput(out NetworkInputData data))
+            {
+                data.direction.Normalize();
+                _cc.Move(5 * data.direction * Runner.DeltaTime);
+            }
+        }
+    }
+}
