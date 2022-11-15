05f79ebf22327c00b47bec8e74330b325eb06b02
roland-vidar <shanlogauthier@gmail.com>
2022-11-14 22:07:57 -0300

    Player Anim 2 states

--- Assets/Player_/Player.cs
+++ Assets/Player_/Player.cs
@@ -8,7 +8,10 @@ namespace Player_
         [SerializeField] private Bullet _prefabBall;
         [SerializeField] private Transform spawnBulletTransform;
         [SerializeField] private float _playerSpeed;
+        [SerializeField] private Animator _animator;
         [Networked] private TickTimer reloadTime { get; set; }
+        
+        private PlayerAnimationStates currentPlayerAnimationState;
 
         public bool Alive
         {
@@ -25,6 +28,7 @@ namespace Player_
         private void Awake()
         {
             _cc = GetComponent<NetworkCharacterControllerPrototype>();
+            currentPlayerAnimationState = PlayerAnimationStates.Idle;
         }
         
         public override void FixedUpdateNetwork()
@@ -32,6 +36,7 @@ namespace Player_
             if (GetInput(out NetworkInputData data))
             {
                 if (!_canMove) return;
+                UpdateAnim(data);
                 data.direction.Normalize();
                 _cc.Move(_playerSpeed * data.direction * Runner.DeltaTime);
 
@@ -55,9 +60,38 @@ namespace Player_
             }
         }
 
+        private void UpdateAnim(NetworkInputData networkInputData)
+        {
+            PlayerAnimationStates newState = PlayerAnimationStates.Idle;
+
+            Debug.Log($"NetworkInputData.Direction: {networkInputData.direction}");
+            
+            if (networkInputData.direction != Vector3.zero)
+            {
+                newState = PlayerAnimationStates.Walking;
+            }
+            else
+            {
+                newState = PlayerAnimationStates.Idle;
+            }
+
+            if (newState != currentPlayerAnimationState)
+            {
+                Debug.Log($"CurrentState: {newState}");
+                currentPlayerAnimationState = newState;
+                _animator.SetTrigger(currentPlayerAnimationState.ToString());
+            }
+        }
         public void SetMovement(bool value)
         {
             _canMove = value;
         }
+    }
+
+    public enum PlayerAnimationStates
+    {
+        Idle,
+        Walking,
+        Dead
     }
 }
