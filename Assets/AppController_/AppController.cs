6b5cfe14378599a780ee0023ab0d0ff5266a9aec
roland-vidar <shanlogauthier@gmail.com>
2022-11-22 20:14:25 -0300

    Fix nickname when type R

--- Assets/AppController_/AppController.cs
+++ Assets/AppController_/AppController.cs
@@ -5,13 +5,15 @@ namespace AppController_
 {
     public class AppController : MonoBehaviour
     {
+
+        [SerializeField] private GameObject menuUi;
         void Update()
         {
             if (Input.GetKeyUp(KeyCode.Escape))
             {
                 Application.Quit();
             }
-            if (Input.GetKeyUp(KeyCode.R))
+            if (Input.GetKeyUp(KeyCode.R) && !menuUi.activeSelf)
             {
                 var currentScene = SceneManager.GetActiveScene().buildIndex;
                 SceneManager.LoadScene(currentScene);
